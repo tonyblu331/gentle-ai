ea6ea75cb98a1fd8764c18c8eb403fce3d99991d
Antonio Bonet <tonybonet31@yahoo.es>
2026-05-19 14:12:49 +0700

    fix(test): make command output test portable

--- internal/cli/command_output_test.go
+++ internal/cli/command_output_test.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"fmt"
+	"os"
 	"strings"
 	"testing"
 
@@ -10,8 +12,9 @@ import (
 func TestExecuteCommandQuietModeIncludesCapturedOutputOnFailure(t *testing.T) {
 	restore := SetCommandOutputStreaming(false)
 	defer restore()
+	t.Setenv("GO_WANT_EXECUTE_COMMAND_HELPER_PROCESS", "1")
 
-	err := executeCommand("bash", "-c", "echo boom && exit 1")
+	err := executeCommand(os.Args[0], "-test.run=TestExecuteCommandHelperProcess", "--")
 	if err == nil {
 		t.Fatal("executeCommand() error = nil, want non-nil")
 	}
@@ -19,6 +22,14 @@ func TestExecuteCommandQuietModeIncludesCapturedOutputOnFailure(t *testing.T) {
 	if !strings.Contains(err.Error(), "boom") {
 		t.Fatalf("executeCommand() error = %q, want captured output", err.Error())
 	}
+}
+
+func TestExecuteCommandHelperProcess(t *testing.T) {
+	if os.Getenv("GO_WANT_EXECUTE_COMMAND_HELPER_PROCESS") != "1" {
+		return
+	}
+	fmt.Fprintln(os.Stdout, "boom")
+	os.Exit(1)
 }
 
 func TestSetCommandOutputStreamingRestore(t *testing.T) {
